6f4712c4f8d81a402b7f083b98c1403b385ca9a6
Razig17 <razog1999@gmail.com>
2024-03-02 04:12:20 +0300

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i ,j;
+	size_t i, j;
 	int tmp, swap = 5;
 
 	for (j = 0; j < size; j++)
@@ -29,6 +29,6 @@ void bubble_sort(int *array, size_t size)
 			}
 		}
 		if (swap == 0)
-		break;
+			break;
 	}
 }
