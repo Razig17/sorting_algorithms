a42b8361328246096591c5fa611135e01f2a7c15
Razig17 <razog1999@gmail.com>
2024-03-04 03:34:55 +0300

    Shell sort

--- sort.h
+++ sort.h
@@ -26,5 +26,5 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quick_sort_recursion(int array[], size_t lo, size_t hi, size_t size);
 size_t lomuto_pivot(int array[], size_t lo, size_t hi, size_t size);
-
+void shell_sort(int *array, size_t size);
 #endif /* SORT_H*/
