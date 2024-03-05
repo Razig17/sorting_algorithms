db986ad612d4bcf82900399c8905938e7fc3fc18
Razig17 <razog1999@gmail.com>
2024-03-06 02:49:26 +0300

    Quick Sort Hoare Partition

--- sort.h
+++ sort.h
@@ -50,4 +50,7 @@ void bubble(int *array, size_t size, size_t exp);
 void merge_b(int arr[], size_t low, size_t count, int direction);
 void bitonic(int arr[], size_t low, size_t count, size_t size, int direction);
 void bitonic_sort(int *array, size_t size);
+
+void quick_sort_hoare(int *array, size_t size);
+void hoare_pivot(int *arr, int low, int high, size_t size);
 #endif /* SORT_H*/
